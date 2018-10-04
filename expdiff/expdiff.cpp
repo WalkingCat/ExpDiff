219d4093b2db65daa64461583df165de967e59a4
WalkingCat <walking.cat@hotmail.com>
2018-10-05 02:10:32 +0800

    supports find files recursively

--- expdiff/expdiff.cpp
+++ expdiff/expdiff.cpp
@@ -6,6 +6,7 @@ enum diff_options {
 	diffNone = 0x0,
 	diffOld = 0x1,
 	diffNew = 0x2,
+	diffRec = 0x4,
 	diffWcs = 0x8,
 	diffOutput = 0x40000000,
 	diffHelp = 0x80000000,
@@ -15,6 +16,7 @@ const struct { const wchar_t* arg; const wchar_t* arg_alt; const wchar_t* params
 	{ L"?",		L"help",			nullptr,		L"show this help",						diffHelp },
 	{ L"n",		L"new",				L"<filename>",	L"specify new file(s)",					diffNew },
 	{ L"o",		L"old",				L"<filename>",	L"specify old file(s)",					diffOld },
+	{ L"r",		L"recursive",		nullptr,		L"search folder recursively",			diffRec },
 	{ nullptr,	L"wcs",				nullptr,		L"folder is Windows Component Store",	diffWcs },
 	{ L"O",		L"out",				L"<filename>",	L"output to file",						diffOutput },
 };
@@ -28,8 +30,7 @@ void print_usage() {
 		if (o->arg_alt != nullptr) {
 			len = wcslen(o->arg_alt);
 			printf_s("\t--%S", o->arg_alt);
-		}
-		else printf_s("\t");
+		} else printf_s("\t");
 
 		if (len < 6) printf_s("\t");
 
@@ -59,8 +60,7 @@ int wmain(int argc, wchar_t* argv[])
 				for (auto o = begin(cmd_options); o != end(cmd_options); ++o) {
 					if ((o->arg_alt != nullptr) && (wcscmp(arg + 2, o->arg_alt) == 0)) { curent_option = o->options; }
 				}
-			}
-			else {
+			} else {
 				for (auto o = begin(cmd_options); o != end(cmd_options); ++o) {
 					if ((o->arg != nullptr) && (wcscmp(arg + 1, o->arg) == 0)) { curent_option = o->options; }
 				}
@@ -72,19 +72,16 @@ int wmain(int argc, wchar_t* argv[])
 				if (curent_option == diffNew) {
 					if ((i + 1) < argc) new_files_pattern = argv[++i];
 					else valid = false;
-				}
-				else if (curent_option == diffOld) {
+				} else if (curent_option == diffOld) {
 					if ((i + 1) < argc) old_files_pattern = argv[++i];
 					else valid = false;
-				}
-				else if (curent_option == diffOutput) {
+				} else if (curent_option == diffOutput) {
 					if ((i + 1) < argc) output_file = argv[++i];
 					else valid = false;
 				} else options = (options | curent_option);
 			}
 			if (!valid && (err_arg == nullptr)) err_arg = arg;
-		}
-		else { if (new_files_pattern.empty()) new_files_pattern = arg; else err_arg = arg; }
+		} else { if (new_files_pattern.empty()) new_files_pattern = arg; else err_arg = arg; }
 	}
 
 	if ((new_files_pattern.empty() && old_files_pattern.empty()) || (err_arg != nullptr) || (options & diffHelp)) {
@@ -111,8 +108,7 @@ int wmain(int argc, wchar_t* argv[])
 		if (((options & diffWcs) == diffWcs)) {
 			ret = find_files_wcs_ex(files_pattern);
 		} else {
-			auto files = find_files(files_pattern.c_str());
-			if (!files.empty()) ret[wstring()].swap(files);
+			ret = find_files_ex(files_pattern, (options & diffRec) == diffRec);
 		}
 		fwprintf_s(out, L"%ls\n", !ret.empty() ? L"" : L" (EMPTY!)");
 		return ret;
@@ -199,7 +195,7 @@ int wmain(int argc, wchar_t* argv[])
 
 	fwprintf_s(out, L"\n");
 
-    return 0;
+	return 0;
 }
 
 set<string> load_exports(const wstring & filename)
@@ -211,7 +207,7 @@ set<string> load_exports(const wstring & filename)
 		if (!file.valid()) return;
 
 		IMAGE_DOS_HEADER dos_header = {};
-		if(!file.read(&dos_header)) return;
+		if (!file.read(&dos_header)) return;
 		if (dos_header.e_magic != IMAGE_DOS_SIGNATURE) return;
 		if (!file.reset(dos_header.e_lfanew)) return;
 		DWORD nt_sig = {};
@@ -232,8 +228,7 @@ set<string> load_exports(const wstring & filename)
 			if (!file.read(&opt_header)) return;
 			export_dir_address = opt_header.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress;
 			export_dir_size = opt_header.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].Size;
-		}
-		else if (opt_magic == IMAGE_NT_OPTIONAL_HDR64_MAGIC) {
+		} else if (opt_magic == IMAGE_NT_OPTIONAL_HDR64_MAGIC) {
 			IMAGE_OPTIONAL_HEADER64 opt_header = {};
 			if (!file.read(&opt_header)) return;
 			export_dir_address = opt_header.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress;
